6adf35197b2bd62b187855ecdb7301bf54bf19fd
Ernest <okiyaernest54@gmail.com>
2023-07-03 05:20:12 -0700

    Initial Commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon July 3 2023
+@author: Ernest Okiya 
+"""
+
+
+class Rectangle:
+    """Empty class Rectangle that defines a rectangle figure
+
+    Attributes:
+        empty
+    """
+
+    pass
